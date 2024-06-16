09b963e3e7e0fbd9a80e325ec6fcda8c300d0efe
joe-black-jb <joeblack3601@gmail.com>
2024-06-16 14:44:14 +0900

    型定義ファイルを実装

---
+++ src/types/types.ts
@@ -0,0 +1,42 @@
+export interface Title {
+  Category: string;
+  CompanyID: number;
+  CreatedAt: string;
+  DeletedAt?: string;
+  Depth: number;
+  FiscalYear: number;
+  HasValue: boolean;
+  ID: number;
+  Name: string;
+  StatementType: number;
+  UpdatedAt: string;
+  Value?: string;
+  order: number;
+  parent_title_id: number;
+}
+
+export interface Company {
+  CreatedAt: string;
+  ID: number;
+  Name: string;
+  Titles?: Title[] | null;
+  UpdatedAt: string;
+  Established: string | null;
+  Capital: string | null;
+}
+
+// export interface Value {
+//   Name: string;
+//   Value: string;
+//   Depth: number;
+//   parent_title_id: number;
+// }
+
+export interface TitleByDepth {
+  [key: number]: Title[];
+}
+
+export interface TitleFamily {
+  parent: string;
+  child: Title[];
+}
