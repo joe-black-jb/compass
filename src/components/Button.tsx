c32c7e088aad77856b2f90dab8207f9a4d1b051b
joe-black-jb <joeblack3601@gmail.com>
2024-06-16 18:10:47 +0900

    ButtonコンポーネントにclassNameを渡せるよう修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,20 @@
+import classNames from "classnames";
 import React from "react";
 
 interface Props {
   label: string;
   onClick?: () => void;
+  className?: string;
 }
 
 const Button = (props: Props) => {
+  const { className, label, onClick } = props;
+  const defaultClassName =
+    "hover:bg-gray-500 text-gray-700 font-bold py-2 px-4 rounded border-2 border-gray-700";
+  const mergedClassName = classNames(defaultClassName, className);
   return (
-    <button
-      className="hover:bg-gray-500 text-gray-700 font-bold py-2 px-4 rounded border-2 border-gray-700"
-      onClick={props.onClick}
-    >
-      {props.label}
+    <button className={mergedClassName} onClick={onClick}>
+      {label}
     </button>
   );
 };
