5792e2314c1bda4bac4b0d53752ed6ad32f40927
joe-black-jb <joeblack3601@gmail.com>
2024-06-16 14:45:24 +0900

    ルーティング処理を修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,26 @@ import React from "react";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./pages/About";
+import CompanyDetail from "./components/CompanyDetail";
+import CompanyEdit from "./components/CompanyEdit";
 
 function App() {
   return (
     <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer />
+      <div className="m-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/company/:companyId/edit" element={<CompanyEdit />} />
+          <Route path="/company/:companyId" element={<CompanyDetail />} />
+        </Routes>
+      </div>
+      {/* <Footer /> */}
     </>
   );
 }
