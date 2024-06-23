9dfcd5bfba08cd9fa102876eeb822fc474057e95
joe-black-jb <joeblack3601@gmail.com>
2024-06-23 20:39:26 +0900

    ルーディング処理修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./pages/About";
-import CompanyDetail from "./components/CompanyDetail";
-import CompanyEdit from "./components/CompanyEdit";
 import NewTitle from "./pages/NewTitle";
+import CompanyTitleEdit from "./components/CompanyTitleEdit";
+import CompanyDetail from "./components/CompanyDetail";
 
 function App() {
   return (
@@ -18,8 +17,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/company/:companyId/edit" element={<CompanyEdit />} />
           <Route path="/company/:companyId/new/title" element={<NewTitle />} />
+          <Route
+            path="/company/:companyId/title/:titleId/edit"
+            element={<CompanyTitleEdit />}
+          />
           <Route path="/company/:companyId" element={<CompanyDetail />} />
         </Routes>
       </div>
