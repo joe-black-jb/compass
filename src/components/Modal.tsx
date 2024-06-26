8f7b38043c168368509a10a4cf9c698470107ebc
joe-black-jb <joeblack3601@gmail.com>
2024-06-26 18:42:27 +0900

    モーダルをより汎用的に修正

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,29 +6,45 @@ import {
   Transition,
   TransitionChild,
 } from "@headlessui/react";
-import { CheckCircleIcon } from "@heroicons/react/24/outline";
-import { ModalStatus } from "../types/types";
+import {
+  CheckCircleIcon,
+  ExclamationTriangleIcon,
+} from "@heroicons/react/24/outline";
+import { Company, ModalStatus, ResultModalStatus } from "../types/types";
 import { Link, useNavigate, useRoutes } from "react-router-dom";
 
-export default function Modal(props: ModalStatus) {
-  const { status, isOpen, company } = props;
-  const [open, setOpen] = useState(true);
+interface Props {
+  isOpen: boolean;
+  open: boolean;
+  status: ResultModalStatus;
+  company?: Company;
+  dialogTitle?: string;
+  dialogText?: string;
+  proceedText?: string;
+  cancelText?: string;
+  setOpen: (value: boolean) => void;
+  onProceed?: (event?: React.FormEvent) => void;
+  onCancel?: () => void;
+}
 
-  // const router = useRoutes();
-  const navigate = useNavigate();
-  const goToDetail = () => {
-    console.log("goToDetail");
-    navigate(`/company/${company?.ID?.toString()}`);
-    // setOpen(false);
-  };
-  const keepEdit = () => {
-    window.location.reload();
-    setOpen(false);
-  };
+export default function Modal(props: Props) {
+  const {
+    isOpen,
+    open,
+    status,
+    company,
+    dialogTitle,
+    dialogText,
+    proceedText,
+    cancelText,
+    setOpen,
+    onProceed,
+    onCancel,
+  } = props;
 
   return (
     <Transition show={open}>
-      <Dialog className="relative z-10" onClose={setOpen}>
+      <Dialog className="relative z-10" onClose={() => setOpen(false)}>
         <TransitionChild
           enter="ease-out duration-300"
           enterFrom="opacity-0"
@@ -53,46 +69,55 @@ export default function Modal(props: ModalStatus) {
               <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <CheckCircleIcon
-                        className="h-6 w-6 text-green-600"
-                        aria-hidden="true"
-                      />
-                    </div>
+                    {status === "OK" && (
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <CheckCircleIcon
+                          className="h-6 w-6 text-green-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    )}
+                    {status === "NG" && (
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <ExclamationTriangleIcon
+                          className="h-6 w-6 text-red-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    )}
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <DialogTitle
                         as="h3"
                         className="text-base font-semibold leading-6 text-gray-900"
                       >
-                        更新成功
+                        {dialogTitle}
                       </DialogTitle>
                       <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          データの更新に成功しました
-                        </p>
+                        <p className="text-sm text-gray-500">{dialogText}</p>
                       </div>
                     </div>
                   </div>
                 </div>
-                {/* <Link to={`company/${company?.ID?.toString()}/detail`}> */}
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:ml-3 sm:w-auto"
-                    // onClick={() => setOpen(false)}
-                    onClick={goToDetail}
-                  >
-                    会社詳細ページへ
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:mt-0 sm:w-auto"
-                    onClick={keepEdit}
-                    // onClick={() => setOpen(false)}
-                    data-autofocus
-                  >
-                    編集を続ける
-                  </button>
+                  {cancelText && (
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:mt-0 sm:w-auto"
+                      onClick={onCancel}
+                      data-autofocus
+                    >
+                      {cancelText}
+                    </button>
+                  )}
+                  {proceedText && (
+                    <button
+                      type="button"
+                      className="mr-3 mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:mt-0 sm:ml-3 sm:w-auto"
+                      onClick={onProceed}
+                    >
+                      {proceedText}
+                    </button>
+                  )}
                 </div>
                 {/* </Link> */}
               </DialogPanel>
