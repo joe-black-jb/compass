be53b0b4a8b0147465644cd0e8df22695662cb73
joe-black-jb <joeblack3601@gmail.com>
2024-11-02 02:47:02 +0900

    ヘッダーの修正

--- src/components/Header2.tsx
+++ src/components/Header2.tsx
@@ -1,17 +1,23 @@
 export default function Header2() {
   return (
-    <header className="fixed top-0 w-full z-10 bg-green-400">
+    <header className="fixed top-0 h-[56px] w-full z-10 bg-green-400">
       <nav
-        className="relative mx-auto flex justify-between px-6 py-4 lg:px-8"
+        className="relative mx-auto flex justify-between"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
+          {/* <a href="/" className="-m-1.5 p-1.5">
             <img
               src={`${process.env.PUBLIC_URL}/compass-logo.png`}
-              className="w-6 h-6"
+              className="w-6 h-6 mt-4"
               alt="Logo"
             />
+          </a> */}
+          <a
+            href="/"
+            className="px-2 text-gray-600 h-[56px] hover:bg-green-300 rounded-lg font-bold content-center"
+          >
+            Compass 〜財務諸表データ検索アプリ〜
           </a>
         </div>
       </nav>
