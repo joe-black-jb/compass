474af74b80999792fc947259a9be971f3f16fe30
joe-black-jb <joeblack3601@gmail.com>
2024-11-03 23:51:10 +0900

    API base_url の設定を修正

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,16 +1,9 @@
 import axios from "axios";
-import { config } from "./config/config";
 
-const env = process.env.NODE_ENV || "development";
-
-let baseUrl = "";
-if (env === "development") {
-  baseUrl = process.env.REACT_APP_API_BASE_URL || "";
-  // axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
-  axios.defaults.headers.post["Content-Type"] =
-    "application/json;charset=utf-8";
-  axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-}
+const baseUrl = process.env.REACT_APP_API_BASE_URL || "";
+// axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
 const api = axios.create({
   baseURL: baseUrl,
   headers: {
