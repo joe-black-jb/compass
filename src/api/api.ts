5bef4f4e121a392ea75ba18f0a127c5b6eaeeeb5
joe-black-jb <joeblack3601@gmail.com>
2024-11-08 22:52:20 +0900

    検索用関数の作成

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,16 @@
-import { NewsList } from "../types/types";
+import { Company, NewsList } from "../types/types";
 import api from "./axiosConfig";
 
 export const getLatestNews = async (): Promise<NewsList | undefined> => {
   const result = await api.get("/public/news");
   return result?.data as NewsList;
+};
+
+export const searchCompanies = async (
+  companyName: string
+): Promise<Company[]> => {
+  const result = await api.get("/private/search", {
+    params: { companyName },
+  });
+  return (result?.data as Company[]) || [];
 };
