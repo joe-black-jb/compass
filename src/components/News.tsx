7ba573419ea0b530d295ca2f0b0adcc9ff737527
joe-black-jb <joeblack3601@gmail.com>
2024-11-10 00:28:39 +0900

    ニュースのロード中にスピナーを表示

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -2,20 +2,32 @@ import React, { useEffect, useState } from "react";
 import { getLatestNews } from "../api/api";
 import { NewsList } from "../types/types";
 import NewsCard from "./NewsCard";
+import LoadingIcon from "./LoadingIcon";
 
 const News = () => {
   const [news, setNews] = useState<NewsList>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     fetchLatestNews();
   }, []);
 
   const fetchLatestNews = async () => {
+    setIsLoading(true);
     const newsList = await getLatestNews();
     if (newsList) {
       setNews(newsList);
+      setIsLoading(false);
     }
   };
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center mt-20">
+        <LoadingIcon />
+      </div>
+    );
+  }
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
