cc4e600f237678dfd3ac1af278e4c02bc8aa8ef5
joe-black-jb <joeblack3601@gmail.com>
2024-11-12 12:21:48 +0900

    ニュースのロード中にスケルトンを表示する

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -2,38 +2,46 @@ import React, { useEffect, useState } from "react";
 import { getLatestNews } from "../api/api";
 import { NewsList } from "../types/types";
 import NewsCard from "./NewsCard";
-import LoadingIcon from "./LoadingIcon";
+import NewsCardSkelton from "./NewsCardSkelton";
 
 const News = () => {
   const [news, setNews] = useState<NewsList>();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  // ダミー配列 (length = 10)
+  const dummyNews = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 
   useEffect(() => {
     fetchLatestNews();
   }, []);
 
   const fetchLatestNews = async () => {
-    setIsLoading(true);
     const newsList = await getLatestNews();
     if (newsList) {
-      setNews(newsList);
-      setIsLoading(false);
+      setTimeout(() => {
+        setNews(newsList);
+        setIsLoading(false);
+      }, 3000);
     }
   };
 
-  if (isLoading) {
-    return (
-      <div className="flex justify-center items-center mt-20">
-        <LoadingIcon />
-      </div>
-    );
-  }
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      {news?.news_list &&
-        news.news_list.length > 0 &&
-        news.news_list.map((news) => <NewsCard key={news.link} news={news} />)}
+      {isLoading ? (
+        <>
+          {dummyNews.map((dummy, index) => (
+            <NewsCardSkelton key={index} />
+          ))}
+        </>
+      ) : (
+        <>
+          {news?.news_list &&
+            news.news_list.length > 0 &&
+            news.news_list.map((news) => (
+              <NewsCard key={news.link} news={news} />
+            ))}
+        </>
+      )}
     </div>
   );
 };
---
+++ src/components/NewsCardSkelton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NewsCardSkelton = () => {
+  return (
+    <div className="h-[100px] mb-4 bg-gray-100 animate-pulse xs:max-w-sm overflow-hidden rounded-t-lg border-b-[1px] border-gray-400 mt-2 mr-2 ml-2 pb-2"></div>
+  );
+};
+
+export default NewsCardSkelton;
